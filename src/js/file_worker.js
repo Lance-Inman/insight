ad84131d859e6f63aa65a3c85a4cf63ade4d3430
Keefer Sands <keefer212@gmail.com>
2019-05-14 15:57:37 -0600

    Messing with webworker

--- src/js/file_worker.js
+++ src/js/file_worker.js
@@ -4,9 +4,9 @@ function readFile(files){
     if(files.length > 0){
         let current_index = 0;
         fr.onload = function () {
-            console.log(current_index);
             var data = fr.result;
             var array = new Int8Array(data);
+            parseFile(decoder.decode(array));
             current_index++;
             if(current_index < files.length){
                 fr.readAsArrayBuffer(files[current_index]);
@@ -14,6 +14,17 @@ function readFile(files){
         };
         fr.readAsArrayBuffer(files[current_index]);
     }
+}
+function parseFile(data){
+    data = data.split(/\n/);
+    for(var x = 0;x<data.length;x++){
+        let values = data[x].split("\t");
+        let date = values[0];
+        let gps_status = values[1];
+        let event_code = values[2];
+        let message = values[3];
+    }
+    console.log(data);
 }
 self.addEventListener("message", function(e) {
     console.log("Message Received");
